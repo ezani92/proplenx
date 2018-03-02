ca324bc5d9e75974ec9aeb1d4686c24f5943332f
Shaiful Ezani <shaiful@naxpansion.com>
2018-03-02 15:38:48 +0800

    update

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'staging@naxpansion.com'),
+        'name' => env('MAIL_FROM_NAME', 'Naxpansion Email Staging'),
     ],
 
     /*
